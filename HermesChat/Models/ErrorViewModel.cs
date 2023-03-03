de4cbd63bae8e0d1a04fd6983be2504ebafdbdff
Artiom Sudin <asudin.dev@gmail.com>
2023-03-03 17:54:05 +0300

    Initial commit

---
+++ HermesChat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HermesChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
